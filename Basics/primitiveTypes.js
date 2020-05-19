aeabb2615fddb4d027358a05166bb07de3ad2654
LevanceWam <lkwamley@fullsail.edu>
2020-05-19 11:46:55 -0400

    finished learning about primitive types

---
+++ Basics/primitiveTypes.js
@@ -0,0 +1,16 @@
+// Primitive types
+// In JS we have 2 types Primitives and Reference types we will look at the latter of the 2 later on
+// some primitives we have are:
+/*
+String
+Number
+Boolean
+Undefined
+Null
+*/
+let name = 'vance'; // This is a string or string literal
+let age = 25; // Number literal
+let isApproved = false; // Boolean literal
+let firstName = undefined; // we can also just leave this blank
+let selectColor = null; // Used when we explicitly want to clear the value of a variable.
+// so in the future if we make a color picker and the user chooses red then it will change to red. 
