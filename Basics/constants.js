d20d3ddf1409e982b2e6cca9fde499289edf94c5
LevanceWam <lkwamley@fullsail.edu>
2020-05-19 11:26:32 -0400

    learned about constant

--- Basics/constants.js
+++ Basics/constants.js
@@ -1,3 +1,10 @@
 const interestRate = 0.3;
-interestRate = 1;
+// interestRate = 1; Adding this will cause a error 
 console.log(interestRate)
+
+// There are times we have a variable value that we do not want to change.
+// This is because it can cause a bug in the program
+// To avoid this we use const.
+// We can not reassign a value on a constant 
+// Following best practices if you need to reassign use let 
+// If you don't then use const
