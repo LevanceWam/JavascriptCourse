ecb4563e89827bc4f996fd7eafb443e8ab7d30f2
LevanceWam <lkwamley@fullsail.edu>
2020-05-20 23:11:16 -0400

    Completed the exercise

---
+++ Operators/exercise.js
@@ -0,0 +1,18 @@
+// let's write a program that will swap the values of variables
+let a = 'red';
+let b = 'blue';
+
+console.log('a is currently: ' + a);
+console.log('b is currently: ' + b);
+
+function swappingColors(x, y) {
+    // set a variable to hold x value so y can take it from there
+    let temp = x;
+    x = y;
+    y = temp;
+
+    console.log('a is currently: ' + x)
+    console.log('b is currently: ' + y)
+}
+
+swappingColors(a, b);
