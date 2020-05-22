427064af264ffb1b6063fba0acf58a5b60b7ad49
LevanceWam <lkwamley@fullsail.edu>
2020-05-22 11:01:33 -0400

    learned about switch and case

--- Control Flow/ifElse.js
+++ Control Flow/ifElse.js
@@ -2,11 +2,12 @@
 // If hour is between 6am and 12pm: good morning 
 // If it is between 12pm and 6pm: Good afternoon!
 // Otherwise: Good evening!
-let hour = 20;
-if (hour >= 6 && hour < 12) {
+let hour = 12;
+// condition to run the code.
+if (hour >= 6 && hour < 12)
+    // code that is executed if condition was met.
     console.log("Good morning!");
-} else if (hour >= 12 && hour < 18) {
-    console.log("Good Afternoon!")
-} else {
-    console.log("Good Evening")
-}
+else if (hour >= 12 && hour < 18)
+    console.log("Good Afternoon!");
+else
+    console.log("Good Evening");
