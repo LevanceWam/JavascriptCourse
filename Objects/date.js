8abef3173e12288f4b7f36963c98e576cd280f65
LevanceWam <lkwamley@fullsail.edu>
2020-05-27 23:10:57 -0400

    learend abiut date

---
+++ Objects/date.js
@@ -0,0 +1,7 @@
+// we are gonna look at the Date object
+const now = new Date();
+const date1 = new Date('May 11 2019 09:00');
+const date2 = new Date(2013, 3, 23, 9);
+
+now.setFullYear(2017);
+consolr
