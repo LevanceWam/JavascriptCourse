648e062088b02a6ab45dadbaca2b17646f25729d
LevanceWam <lkwamley@fullsail.edu>
2020-06-12 23:23:00 -0400

    quick update

---
+++ ObjectsII/exerciseStopwatch.js
@@ -0,0 +1,32 @@
+// Exercise
+// Design a stopwatch obj
+// when we define a new const and set it to stopwatch
+// in that stopwatch we have a few members 
+// duration this is a property
+// reset method
+// start method
+// stop method
+
+// intitally duration is 0
+// we can start the watch
+// it we do it again it will throw an error
+
+// we can call stop but just like start if it already is at stop
+// yell at us
+
+// when we call duration after stop it should show us the seconds that have passed 
+// reset sets us back to 0
+
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.startTime = function () {
+        if ()
+    };
+    this.endTime = function () {
+
+    };
+    this.reset = function () {
+
+    };
+}
