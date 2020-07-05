4fe47bfc759f5c2c1402d8c82771519ad36a421f
LevanceWam <lkwamley@fullsail.edu>
2020-07-05 13:51:34 -0400

    learned how to export with node and commonjs

--- ES6Tooling/circle.js
+++ ES6Tooling/circle.js
@@ -1,10 +1,27 @@
 // This is the circle module for commonJsModule.js 
 
+// here is something interesting to know
+// here in our module we are only exporting the circle class 
+// so the radius weakmap is not accessible in our other module
+// this is part of the implementation detail of the circle module 
+// what we are exporting which is our circle class is what we call the public interface 
+// radius is the implementation detail 
+// this is abstraction in pratice we are hiding the details or the complexity inside of the module
+
+// lets say tomorrow we may decide to replace this weakmap with a symbol or a different object
+// that change is not going to breaknthe rest of the application because nowhere
+// esle we have code that touches this radius object
+// it is part of the implementation detail of this module
+
+
 // by default everything we define in a module is considered to be private
 // so it won't be accessible to the outside unless we explicitly export it 
 
+// implementation detail
 const _radius = new WeakMap();
 
+
+// Public Interface 
 class Circle {
     constructor(radius) {
         _radius.set(this, radius);
@@ -35,4 +52,15 @@ class Circle {
 // but this implementation we are exporting a single object
 // so lets try and simplfy it 
 
+// so instead of adding a circle property to module.exports object 
+// we can just reset the object to circle 
+// now when we import the circle module we get the circle class 
+// lets go back to commonJsModule.js
+
+// so module.exports represents the object that is exported from this module.
+// in this case that module is the circle class
+
 module.exports = Circle;
+
+// here is something interesting to know
+// here in our modulw we are only
--- ES6Tooling/commonJsModules.js
+++ ES6Tooling/commonJsModules.js
@@ -9,6 +9,25 @@
 // Lets take the weakmap and the object and place them into a new file called circle.js
 // first we are going to copy and cut the code from here
 
+// on the top of the page we are going to import the circle module 
+// we use the require function for this task
+// the require function is a part of the commonjs format
+// so common js format defines this require and module.exports
+// these are the syntax used on commonjs format 
+// inside the require we want to pass a relative path to the target module
+
+// we use "./ to refer to the current folder and then add circle"
+// note that we dont need to use the extension we don't need to add circle.js 
+// it is assumed by default
+
+// now when we require this we get is what is returned from the circle module
+// we can store the result in a constant 
+// then we can use the constant to create new objects
+
+const Circle = require('./circle');
+
 
 const c = new Circle(10);
 c.draw();
+
+// now we can run this application and everything should still work
